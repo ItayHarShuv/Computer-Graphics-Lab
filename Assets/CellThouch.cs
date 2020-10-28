bf21d0658c2c0d3c5f41b394f0eb6b392931a8f7
ItayHarShuv <itay.hsh@gmail.com>
2020-10-28 18:31:33 +0200

    Add unity project

---
+++ Assets/CellThouch.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CellThouch : MonoBehaviour
+{
+    private void OnMouseDown()
+    {
+        //add here what you wont to happend when you click on a cell
+    }
+}
