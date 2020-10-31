82256d20a17b82737f49e36cf16f14ac69a8d502
ItayHarShuv <itay.hsh@gmail.com>
2020-10-31 17:58:05 +0200

    Add unity project 2048

--- 2Dzhed/Assets/Gridmanager.cs
+++ 2Dzhed/Assets/Gridmanager.cs
@@ -2,66 +2,102 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public class Cell
+{
+    public GameObject obj;
+    public int num = 0;
+}
+
 public class Gridmanager : MonoBehaviour
 {
     [SerializeField]
-    private int rows = 8;
-    [SerializeField]
-    private int cols = 5;
-    [SerializeField]
-    private float tileSize = 1;
-
+    private int rows = 5;
+    private int cols;
+    private float cellSize = 1;
 
     // Start is called before the first frame update
     void Start()
     {
         GenerateGrid();   
     }
+
     private void GenerateGrid()
     {
-        //init Arr
-        GameObject[,] gridArr = new GameObject[rows, cols];
-        for (int i = 0; i < rows; i++)
-        {
-            for (int j = 0; j < cols; j++)
-            {
-                gridArr[i, j] = null;
-            }
-        }
-
-        
-
-        GameObject referenceTile = (GameObject)Instantiate(Resources.Load("Cell3"));
-        Sprite.Create(Sprites.Load("Cell3"), new Rect(0.0f, 50.0f, 0.0f, 50.0f), new Vector2(0.5f, 0.5f), 50.0f);
-        referenceTile.transform.localScale = new Vector2(0.5f, 0.5f);
+        cols = rows;
 
 
+        Cell[,] gridArr = new Cell[rows, cols];
         for (int row = 0; row < rows; row++)
         {
             for (int col = 0; col < cols; col++)
             {
-                gridArr[row, col] = (GameObject)Instantiate(referenceTile, transform); ;
+                gridArr[row, col] = new Cell();
+            }
+        }
+        GameObject eReferenceCell = new GameObject("Empty Cell");
+        GameObject fReferenceCell = new GameObject("Filled Cell");
+        Texture2D eCellTex = Resources.Load<Texture2D>("EmptyCell");
+        Texture2D fCellTex = Resources.Load<Texture2D>("FilledCell");
+        SpriteRenderer eCellRenderer = eReferenceCell.AddComponent<SpriteRenderer>();
+        SpriteRenderer fCellRenderer = fReferenceCell.AddComponent<SpriteRenderer>();
 
-                gridArr[row, col].AddComponent<BoxCollider2D> ();
-                gridArr[row, col].AddComponent<CellThouch>();
+        float PPU = 122.0f; //pixel per unit of the original texture
+        float cellScale = 1.0f;
 
-                float posX = col * tileSize;
-                float posY = row * -tileSize;
+        var mainCameraHeight = 2 * Camera.main.orthographicSize;
+        var mainCameraWidth = mainCameraHeight * Camera.main.aspect;
 
-                var height = 2 * Camera.main.orthographicSize;
-                var width = height * Camera.main.aspect;
+        cellScale = mainCameraWidth / rows;
+        cellSize = cellSize * cellScale;
 
-                gridArr[row, col].transform.position = new Vector2(posX, posY);
+        eCellRenderer.sprite = Sprite.Create(eCellTex, new Rect(0.0f, 0.0f, eCellTex.width, eCellTex.height), new Vector2(0.5f, 0.5f), PPU / cellScale);
+        fCellRenderer.sprite = Sprite.Create(fCellTex, new Rect(0.0f, 0.0f, fCellTex.width, fCellTex.height), new Vector2(0.5f, 0.5f), PPU/cellScale);
+
+        gridArr[1, 1].num = 1;
+        gridArr[2, 2].num = 4;
+        for (int row = 0; row < rows; row++)
+        {
+            for (int col = 0; col < cols; col++)
+            {
+                if (gridArr[row, col].num == 0)
+                    gridArr[row, col].obj = Instantiate(eReferenceCell, transform);
+                else
+                {
+                    gridArr[row, col].obj = Instantiate(fReferenceCell, transform);
+                    //DisplayNum(gridArr[row, col].num);
+                }
+
+
+                gridArr[row, col].obj.AddComponent<BoxCollider2D> ();
+                gridArr[row, col].obj.AddComponent<CellThouch>();
+
+                float posX = col * cellSize + 0.74f;
+                float posY = row * -cellSize - 1.5f;
+
+                
+
+                gridArr[row, col].obj.transform.position = new Vector2(posX, posY);
 
             }
         }
 
-        Destroy(referenceTile);
+        Destroy(eReferenceCell);
+        Destroy(fReferenceCell);
 
-        float gridW = cols * tileSize;
-        float gridH = rows * tileSize;
-        transform.position = new Vector2(-(gridW - tileSize) / 2, (gridH - tileSize) / 2);
+        float gridW = cols * cellSize;
+        float gridH = rows * cellSize;
+        transform.position = new Vector2(-(gridW - cellSize) / 2, (gridH - cellSize) / 2);
     }
+
+    /*void DisplayNum(int num)
+    {
+        GameObject newGO = new GameObject("myTextGO");
+        newGO.transform.SetParent(transform);
+
+        Text myText = newGO.AddComponent<Text>();
+        myText.text = "Ta-dah!";
+    }*/
+
     // Update is called once per frame
     void Update()
     {
