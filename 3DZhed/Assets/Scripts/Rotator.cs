c763479e10e5cd3b3cce0d0cd120584385619d3b
ItayHarShuv <itay.hsh@gmail.com>
2020-12-21 10:35:15 +0200

    Add Rotate Buttons

--- 3DZhed/Assets/Scripts/Rotator.cs
+++ 3DZhed/Assets/Scripts/Rotator.cs
@@ -1,9 +1,25 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class Rotator : MonoBehaviour
 {
-    Vector3 point; 
+    public static Rotator Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    Vector3 point;
+    public char dir;
     [SerializeField] float rotationSpeed = 100f;
 
     void Start()
@@ -14,19 +30,19 @@ public class Rotator : MonoBehaviour
     void Update()
     {
         
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.A) || dir == 'A')
         {
             transform.RotateAround(point, Vector3.up, rotationSpeed * Time.deltaTime);
         }
-        else if (Input.GetKey(KeyCode.D))
+        else if (Input.GetKey(KeyCode.D) || dir == 'D')
         {
             transform.RotateAround(point, -Vector3.up, rotationSpeed * Time.deltaTime);
         }
-        if (Input.GetKey(KeyCode.W))
+        if (Input.GetKey(KeyCode.W) || dir == 'W')
         {
             transform.RotateAround(point, -Vector3.left, rotationSpeed * Time.deltaTime);
         }
-        else if (Input.GetKey(KeyCode.S))
+        else if (Input.GetKey(KeyCode.S) || dir == 'S')
         {
             transform.RotateAround(point, Vector3.left, rotationSpeed * Time.deltaTime);
         }
---
+++ 3DZhed/Assets/Scripts/buttons/RotateButton.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class RotateButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    [SerializeField] char dir = 'n';
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Rotator.Instance.dir = dir;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        Rotator.Instance.dir = 'n';
+    }
+}
